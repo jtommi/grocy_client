92263e247ed4fe377470eaae8b94709bbb9708cc
Thomas <johanns.thomas@gmail.com>
2022-04-25 19:18:19 +0200

    Refactoring tests

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,39 +1,25 @@
 import json
-import re
 import unittest
 from unittest.mock import MagicMock, patch
 
-from requests.models import Response
 from src.grocycode import GrocyCode
 from src.product import Product
 
 
-def mocked_request(*args, **kwargs):
-    response_content = None
-    request_url = kwargs.get("url", None)
-    response_file = None
-    if "/api/stock/products/by-barcode/" in request_url:
-        response_file = "product.json"
-    elif re.search(r".*/stock/products/235/entries$", request_url):
-        response_file = "product_stock_entries_open.json"
-    elif re.search(r".*/stock/products/239/entries$", request_url):
-        response_file = "product_stock_entries_not_open.json"
-
-    if response_file:
-        with open(f"tests/responses/{response_file}", "r") as f:
-            response_content = json.load(f)
-    else:
-        response_content = {}
-
-    response = Response()
-    response.status_code = 200
-    response._content = json.dumps(response_content).encode()
-    return response
-
-
 class TestProduct(unittest.TestCase):
-    @patch("src.api_client.requests.post", side_effect=mocked_request)
+    def setUp(self):
+        response_files = {
+            "stock_entries_open": "product_stock_entries_open.json",
+            "stock_entries_not_open": "product_stock_entries_not_open.json",
+        }
+        self.response_data = {}
+        for response, file in response_files.items():
+            with open(f"tests/responses/{file}", "r") as f:
+                self.response_data[response] = json.load(f)
+
+    @patch("src.api_client.requests.post")
     def test_open_product(self, mock_post):
+        mock_post.return_value = MagicMock(status_code=200)
         product = Product(id=235, stock_id="62505f88ea718")
         product.open()
         # Check that get was called with correct parameters
@@ -41,8 +27,9 @@ class TestProduct(unittest.TestCase):
         self.assertIn("/open", mock_post.call_args.kwargs["url"])
         self.assertIn("235", mock_post.call_args.kwargs["url"])
 
-    @patch("src.api_client.requests.post", side_effect=mocked_request)
+    @patch("src.api_client.requests.post")
     def test_open_product_by_barcode(self, mock_post):
+        mock_post.return_value = MagicMock(status_code=200)
         CODE = "grcy:p:1:x624f2505ded59"
         grocycode = GrocyCode(CODE)
         product = grocycode.get_item()
@@ -53,8 +40,9 @@ class TestProduct(unittest.TestCase):
         self.assertIn("by-barcode", mock_post.call_args.kwargs["url"])
         self.assertIn("x624f2505ded59", mock_post.call_args.kwargs["url"])
 
-    @patch("src.api_client.requests.post", side_effect=mocked_request)
+    @patch("src.api_client.requests.post")
     def test_consume_product(self, mock_post):
+        mock_post.return_value = MagicMock(status_code=200)
         product = Product(id=235, stock_id="62505f88ea718")
         product.consume()
         # Check that get was called with correct parameters
@@ -62,8 +50,9 @@ class TestProduct(unittest.TestCase):
         self.assertIn("/consume", mock_post.call_args.kwargs["url"])
         self.assertIn("235", mock_post.call_args.kwargs["url"])
 
-    @patch("src.api_client.requests.post", side_effect=mocked_request)
+    @patch("src.api_client.requests.post")
     def test_consume_product_by_barcode(self, mock_post):
+        mock_post.return_value = MagicMock(status_code=200)
         CODE = "grcy:p:1:x624f2505ded59"
         grocycode = GrocyCode(CODE)
         product = grocycode.get_item()
@@ -74,26 +63,38 @@ class TestProduct(unittest.TestCase):
         self.assertIn("by-barcode", mock_post.call_args.kwargs["url"])
         self.assertIn("x624f2505ded59", mock_post.call_args.kwargs["url"])
 
-    @patch("src.api_client.requests.get", side_effect=mocked_request)
+    @patch("src.api_client.requests.get")
     @patch("src.api_client.requests.post")
     def test_opens_when_not_open(self, mock_post, mock_get):
-        mock_post.return_value = MagicMock(status_code=200, response=json.dumps({}))
+        mock_get.return_value = MagicMock(
+            status_code=200,
+            json=lambda: self.response_data["stock_entries_not_open"],
+        )
+        mock_post.return_value = MagicMock(status_code=200)
         product = Product(id=239, stock_id="62505f88ea718")
         product.open_or_consume()
         mock_post.assert_called()
         self.assertRegex(mock_post.call_args.kwargs["url"], r".*/open$")
 
-    @patch("src.api_client.requests.get", side_effect=mocked_request)
-    @patch("src.api_client.requests.post", side_effect=mocked_request)
+    @patch("src.api_client.requests.get")
+    @patch("src.api_client.requests.post")
     def test_consumes_when_open(self, mock_post, mock_get):
+        mock_get.return_value = MagicMock(
+            status_code=200,
+            json=lambda: self.response_data["stock_entries_open"],
+        )
         mock_post.return_value = MagicMock(status_code=200)
         product = Product(id=235, stock_id="62505f88ea718")
         product.open_or_consume()
         mock_post.assert_called()
         self.assertRegex(mock_post.call_args.kwargs["url"], r".*/consume$")
 
-    @patch("src.api_client.requests.get", side_effect=mocked_request)
+    @patch("src.api_client.requests.get")
     def test_open_or_consume_raises_on_no_entries(self, mock_get):
+        mock_get.return_value = MagicMock(
+            status_code=200,
+            json=lambda: self.response_data["stock_entries_open"],
+        )
         product = Product(id=235, stock_id="non_existing")
         with self.assertRaises(Exception) as exception:
             product.open_or_consume()

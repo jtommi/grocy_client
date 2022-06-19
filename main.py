5753a5be9c4020976f8b50a47880dab3805e0288
Thomas <johanns.thomas@gmail.com>
2022-06-19 20:09:12 +0200

    Added logging and exception handling

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import logging
 import os
+import sys
 
 import serial
 from serial.tools import list_ports
 
-from src.grocycode import GrocyCode
-from src.product import Product
+from src.api_client import APIException
+from src.grocycode import GrocyCode, InvalidGrocyCodeException
+from src.ntfy import NtfyClient
+from src.product import NoStockEntriesException, Product, ProductNotExistsException
 
-VID_PID = os.getenv("VID_PID")
+VID_PID = os.getenv("VID_PID", "")
 
 
 def find_barcode_scanner(vid_pid: str):
@@ -15,6 +19,9 @@ def find_barcode_scanner(vid_pid: str):
     """
     ports = list_ports.grep(vid_pid)
     device = list(ports)[0]
+    if not device:
+        raise ValueError("Barcode scanner not found")
+
     return device.device
 
 
@@ -24,14 +31,34 @@ def main():
     """
     device = find_barcode_scanner(VID_PID)
 
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter("%(asctime)s [%(name)] %(levelname)-8s %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.WARNING)
+
+    ntfy = NtfyClient()
+
     with serial.Serial(device, 19200, timeout=0) as ser:
         while True:
             barcode = ser.readline()
             barcode = barcode.strip()
             if len(barcode) > 0:
-                grocycode = GrocyCode(barcode)
-                product: Product = grocycode.get_item()
-                product.open_or_consume()
+                try:
+                    grocycode = GrocyCode(barcode)
+                    product: Product = grocycode.get_item()
+                    product.open_or_consume()
+                except (
+                    InvalidGrocyCodeException,
+                    NoStockEntriesException,
+                    ProductNotExistsException,
+                ) as e:
+                    logger.warning(str(e))
+                    ntfy.send_message(str(e))
+                except APIException as e:
+                    logger.exception(str(e))
+                    ntfy.send_message(f"Unhandled API exception: {str(e)}")
 
 
 if __name__ == "__main__":

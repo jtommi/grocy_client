bf1599a67eb0357c1f7e0cbd595317bc5006d191
Thomas <johanns.thomas@gmail.com>
2022-06-26 14:01:40 +0200

    Refactoring

--- main.py
+++ main.py
@@ -10,26 +10,51 @@ from src.grocycode import GrocyCode, InvalidGrocyCodeException
 from src.ntfy import NtfyClient
 from src.product import NoStockEntriesException, Product, ProductNotExistsException
 
-VID_PID = os.getenv("VID_PID", "")
 
-
-def find_barcode_scanner(vid_pid: str):
+class SerialDeviceNotFoundException(Exception):
     """
-    Find barcode scanner
+    Exception raised when serial device is not found
     """
-    ports = list_ports.grep(vid_pid)
-    device = list(ports)[0]
-    if not device:
-        raise ValueError("Barcode scanner not found")
 
-    return device.device
+    pass
+
+
+def find_serial_device(vid_pid: str):
+    """
+    Find serial device
+    """
+    ports = list(list_ports.grep(vid_pid))
+    try:
+        vid, pid = vid_pid.split(":")
+        # Convert vid and pid to integers to match the output of list_ports
+        vid = int("0x" + vid, 16)
+        pid = int("0x" + pid, 16)
+    except ValueError as e:
+        if str(e).startswith("too many values to unpack") or str(e).startswith(
+            "not enough values to unpack"
+        ):
+            raise ValueError(
+                f'vid_pid must be in the format VID:PID. Provided was "{vid_pid}"'
+            ) from None
+        elif str(e).startswith("invalid literal for int() with base 16"):
+            raise ValueError(
+                f'VID or PID is not in hexadecimal format. VID:PID provided was "{vid_pid}"'
+            ) from None
+        else:
+            raise
+
+    for port in ports:
+        if port.vid == vid and port.pid == pid:
+            return port.device
+
+    raise SerialDeviceNotFoundException(f"Serial device {vid_pid} not found")
 
 
 def main():
     """
     Main function
     """
-    device = find_barcode_scanner(VID_PID)
+    device = find_serial_device(os.getenv("VID_PID", ""))
 
     logger = logging.getLogger(__name__)
     handler = logging.StreamHandler(sys.stdout)
